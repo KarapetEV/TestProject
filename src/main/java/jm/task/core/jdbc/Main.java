6374f1652e0ff14cef0c7327902f5fce69ea38f2
Evgeniy Karapyshev <karapet2011@gmail.com>
2021-10-16 19:31:33 +0300

    Добавлено переиспользование методов класса UserDaoJDBCImpl в классе UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,33 +1,36 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Connection conn = Util.getConnection();
 
-        UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
-        dao.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
 
-        dao.saveUser("Вася", "Петров", (byte) 18);
-        dao.saveUser("Иван", "Сидоров", (byte) 25);
-        dao.saveUser("Антон", "Серёгин", (byte) 43);
-        dao.saveUser("Сергей", "Семёнов", (byte) 15);
+        userService.saveUser("Вася", "Петров", (byte) 18);
+        userService.saveUser("Иван", "Сидоров", (byte) 25);
+        userService.saveUser("Антон", "Серёгин", (byte) 43);
+        userService.saveUser("Сергей", "Семёнов", (byte) 15);
 
-        List<User> users = dao.getAllUsers();
+        List<User> users = userService.getAllUsers();
 
         for (User user : users) {
             System.out.println(user.toString());
         }
 
-        dao.cleanUsersTable();
-        dao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
         Util.closeConnection(conn);
     }
